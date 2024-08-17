85b5f1243077c7c6b3db70669fac093a655a5534
dyuri-de <dyuri-de@student.42sp.org.br>
2024-08-17 19:32:58 -0300

    rush00: cantos manipulados

--- rush00/main.c
+++ rush00/main.c
@@ -13,14 +13,23 @@ int main()
 void rush(int x, int y)
 {
 	int mid_x = 0;
-	
+
 	while(mid_x < x)
-	{ 
-		int mid_y = 0;
+	{
+		int mid_y = 0;	
 		while(mid_y < y)
 		{
-			if(mid_x == 0 || mid_y == 0 || mid_x == x || mid_y == y)
-				ft_putchar('o');
+
+			if (mid_x == 0 && mid_y == 0)
+				ft_putchar('x');
+			else if (mid_x == 0 && mid_y == y - 1)
+				ft_putchar('x');
+			else if (mid_x == x - 1 && mid_y == 0)
+				ft_putchar('x');
+			else if (mid_x == x - 1&& mid_y == y - 1)
+				ft_putchar('x');
+			else
+				ft_putchar('x');
 			mid_y++;
 		}
 		write(1, "\n", 1);
