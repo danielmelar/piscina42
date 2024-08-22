736fb9cdd61cfb6b33a0a48e393cd496c78897f6
dyuri-de <dyuri-de@student.42sp.org.br>
2024-08-21 21:01:46 -0300

    update

--- C-Piscine-C-01/ex07/ft_rev_int_tab.c
+++ C-Piscine-C-01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: dyuri-de <@student.42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 19:02:48 by dyuri-de          #+#    #+#             */
-/*   Updated: 2024/08/21 20:44:46 by dyuri-de         ###   ########.fr       */
+/*   Updated: 2024/08/21 21:00:49 by dyuri-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,12 +16,14 @@ void	ft_rev_int_tab(int *tab, int size);
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int	position;
+	int	*decrement;
 
 	position = 0;
+	decrement = &size - 1;
 	while (tab <= 0)
 	{
-		tab[position] = size - 1;
-		size--;
+		tab[position] = *decrement;
+		decrement--;
 		position++;
 
 	}
